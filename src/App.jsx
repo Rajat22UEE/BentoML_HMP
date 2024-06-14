8242b3904960956f274043232a3bdfe96209fb26
Rajat22UEE <rajatnath6909@gmail.com>
2024-06-14 21:00:32 +0530

    !st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx is exported to main.jsx. use route and router
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Make components and pages</div>
+    </>
+  )
+}
+
+export default App
